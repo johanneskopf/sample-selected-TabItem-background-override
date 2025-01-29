90c3a855d546fb02d2e95ba93857003ef8ad60ff
johanneskopf <johanneskopf@gmx.at>
2025-01-29 09:47:59 +0100

    feat: add empty net9 WPF project

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace sample_selected_TabItem_background_override
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
